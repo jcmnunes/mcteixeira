89c71b5a730048de97c31a334d3d30d30c45d866
José Nunes <josenunes34@gmail.com>
2017-12-19 10:08:16 +0000

    Implement work section

--- data/work.js
+++ data/work.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import img1Thumb from '../images/img1-thumb-800x500-squashed.jpg';
 import img2Thumb from '../images/img2-thumb-800x500-squashed.jpg';
 import img3Thumb from '../images/img3-thumb-800x500-squashed.jpg';
@@ -12,18 +13,31 @@ export default [
   {
     key: '1',
     page: '/work-1',
+    header: 'Ultricies Commodo Ullamcorper',
+    text:
+      'Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare vel eu leo. Curabitur blandit tempus porttitor.',
+    date: 'Jun 2016',
     thumb: img1Thumb,
     images: [img1, img2, img3],
   },
   {
     key: '2',
     page: '/work-1',
+    header: 'Ligula Aenean Mattis',
+    text:
+      'Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Cras mattis consectetur purus sit amet fermentum.',
+    date: 'Sep 2016',
     thumb: img2Thumb,
     images: [img1, img2, img3],
   },
   {
     key: '3',
     page: '/work-1',
+    header:
+      'Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+    text:
+      'Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare vel eu leo.',
+    date: 'Nov 2017',
     thumb: img3Thumb,
     images: [img1, img2, img3],
   },
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Lightbox from 'react-image-lightbox';
 import PropTypes from 'prop-types';
-import VisibilitySensor from 'react-visibility-sensor';
 import { navigateTo } from 'gatsby-link';
 import Icon from '../common/Icon';
 import colors from '../../assets/colors';
@@ -20,56 +19,55 @@ class Work extends React.Component {
     const { photoIndex, isOpen } = this.state;
     const { data } = this.props;
     return (
-      <VisibilitySensor onChange={this.onVisibilityChange} partialVisibility>
-        <div className={styles.work}>
-          <div className={styles.thumbnail}>
-            <img src={data.thumb} alt="" />
-            <div className={styles.backdrop} />
-            <div className={styles.links}>
-              <div onClick={() => this.setState({ isOpen: true })}>
-                <Icon icon="image" />
-              </div>
-              <div onClick={() => navigateTo(data.page)}>
-                <Icon icon="link" />
-              </div>
+      <div className={styles.work}>
+        <div className={styles.thumbnail}>
+          <img src={data.thumb} alt="" />
+          <div className={styles.backdrop} />
+          <div className={styles.links}>
+            <div onClick={() => this.setState({ isOpen: true })}>
+              <Icon icon="image" />
             </div>
-          </div>
-
-          {isOpen && (
-            <Lightbox
-              mainSrc={data.images[photoIndex]}
-              nextSrc={data.images[(photoIndex + 1) % data.images.length]}
-              prevSrc={
-                data.images[
-                  (photoIndex + data.images.length - 1) % data.images.length
-                ]
-              }
-              onCloseRequest={() => this.setState({ isOpen: false })}
-              onMovePrevRequest={() =>
-                this.setState({
-                  photoIndex:
-                    (photoIndex + data.images.length - 1) % data.images.length,
-                })
-              }
-              onMoveNextRequest={() =>
-                this.setState({
-                  photoIndex: (photoIndex + 1) % data.images.length,
-                })
-              }
-            />
-          )}
-
-          <div className={styles.title}>
-            <h4>Aliquam eratac</h4>
-            <div
-              className={styles.likesContainer}
-              onClick={() => navigateTo(data.page)}
-            >
-              <Icon icon="circleRight" color={colors.text20} spin={false} />
+            <div onClick={() => navigateTo(data.page)}>
+              <Icon icon="link" />
             </div>
           </div>
         </div>
-      </VisibilitySensor>
+
+        {isOpen && (
+          <Lightbox
+            mainSrc={data.images[photoIndex]}
+            nextSrc={data.images[(photoIndex + 1) % data.images.length]}
+            prevSrc={
+              data.images[
+                (photoIndex + data.images.length - 1) % data.images.length
+              ]
+            }
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex:
+                  (photoIndex + data.images.length - 1) % data.images.length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % data.images.length,
+              })
+            }
+          />
+        )}
+
+        <h4>{data.header}</h4>
+        <div className={styles.date}>
+          <Icon icon="calendar" color={colors.text40} spin={false} />
+          <span className={styles.dateText}>{data.date}</span>
+        </div>
+        <p>{data.text}</p>
+        <div className={styles.readmore} onClick={() => navigateTo(data.page)}>
+          <span className={styles.text}>read more</span>
+          <Icon icon="arrowRight2" color={colors.text} spin={false} />
+        </div>
+      </div>
     );
   }
 }
