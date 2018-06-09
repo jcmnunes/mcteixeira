7fb585978d06aad9aa2478c46a37097daefb2b27
José Nunes <josenunes34@gmail.com>
2018-06-09 18:08:39 +0100

    #33 Add gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     'gatsby-plugin-react-next',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-transformer-remark',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
