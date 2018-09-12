1b833393e61b07d0e539ea9a471318f785ef19a7
Jose Nunes <josenunes34@gmail.com>
2018-09-12 19:16:20 +0100

    Testing...123

--- src/components/common/ArrowButton/ArrowButton.js
+++ src/components/common/ArrowButton/ArrowButton.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import Icon from '../Icon';
 
-import colors from '../../../assets/colorszzz';
+import colors from '../../../assets/colorszz';
 import styles from './ArrowButton.module.css';
 
 const Text = ({ text, direction }) => (
